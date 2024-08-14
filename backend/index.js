8110c0b27a6f6f7edc450a8e177df1bd6fd40a08
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-08-14 20:35:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ dotenv.config({
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: "*", // Allow all origins
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 
 //--------------------------------------------------------
 const authRoute = require("./routes/authRoute.js");
