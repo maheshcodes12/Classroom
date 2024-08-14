77c70de4ed069505a11825004efabb81194dae3a
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-08-14 21:03:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
 	cors({
-		origin: "*", // Allow all origins
+		origin: "https://classroom-two-dun.vercel.app/", 
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 	})
