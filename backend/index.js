d2c08348309d01b2853f19e115fffdc08465dffb
Mahesh Suryawanshi <140623030+maheshcodes12@users.noreply.github.com>
2024-08-14 21:12:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(
 		origin: "https://classroom-two-dun.vercel.app/", 
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
 	})
 );
 
